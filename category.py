cb435038a93bc56fe4425ee033a3485a46371e12
grayd <sharin@you-x.ru>
2024-07-25 13:38:40 +0300

    init

---
+++ category.py
@@ -0,0 +1,31 @@
+class Category:
+    def __init__(self, name):
+        self.name = name
+        self._products = []
+
+    def add_product(self, product):
+        self._products.append(product)
+
+    @property
+    def products(self):
+        return [f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт." for product in self._products]
+
+class Product:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.__price = price
+        self.quantity = quantity
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            raise ValueError("Цена должна быть больше нуля.")
+        self.__price = new_price
+
+    @classmethod
+    def new_product(cls, product_data):
+        return cls(product_data['name'], product_data['price'], product_data['quantity'])
---
+++ product.py
@@ -0,0 +1,7 @@
+class Product:
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+        #self.color = color
