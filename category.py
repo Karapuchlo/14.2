1966e1fecd3683a6f365053e869ec5240abee8bb
grayd <sharin@you-x.ru>
2024-08-05 11:49:03 +0300

    добавил еще добаление товаров добавил 2 категории и добавил тесты

--- category.py
+++ category.py
@@ -1,11 +1,23 @@
+from product import *
+
 class Category:
+    product_count = None
+
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self._products = products
+        self.products = products
+        self._product_count = len(products)
 
     def add_product(self, product):
-        self._products.append(product)
+        if not isinstance(product, Product) and not issubclass(type(product), Product):
+            raise TypeError("Можно добавлять только продукты или их наследников")
+        self.products.append(product)
+        self._product_count += 1
+
+    @staticmethod
+    def get_product_count(category):
+        return category._product_count
 
     @property
     def products(self):
@@ -15,6 +27,10 @@ class Category:
         total_quantity = sum(product.quantity for product in self._products)
         product_list = "\n".join(self.products)
         return f"{self.name}, {self.description}, количество продуктов: {total_quantity} шт."
+
+    @products.setter
+    def products(self, value):
+        self._products = value
 
 
 
--- test_product.py
+++ test_product.py
@@ -1,5 +1,5 @@
 import pytest
-from product import Product
+from product import *
 from category import Category
 
 def test_product_string_representation():
@@ -20,6 +20,32 @@ def test_product_addition():
     product2 = Product("Banana", 'бананы', 50, 10)
     total_value = product1 + product2
     assert total_value == 900
+
+def test_smartphone_creation():
+    smartphone = Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5, "S23 Ultra", 256, "Серый")
+    assert smartphone.name == "Samsung Galaxy S23 Ultra"
+    assert smartphone.description == "256GB, Серый цвет, 200MP камера"
+    assert smartphone.price == 180000.0
+    assert smartphone.quantity == 5
+    assert smartphone.model == "S23 Ultra"
+    assert smartphone.color == "Серый"
+
+def test_smartphone_str_representation():
+    smartphone = Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5, "S23 Ultra", 256, "Серый")
+    assert str(smartphone) == "Samsung Galaxy S23 Ultra, 180000.00 руб. Остаток: 5 шт."
+
+def test_smartphone_addition():
+    smartphone1 = Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5, "S23 Ultra", 256, "Серый")
+    smartphone2 = Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space")
+    smartphone_sum = smartphone1 + smartphone2
+    assert isinstance(smartphone_sum, Smartphone)
+    assert smartphone_sum.price == 390000.0
+    assert smartphone_sum.quantity == 13
+    assert smartphone_sum.name == "Samsung Galaxy S23 Ultra + Iphone 15"
+    assert smartphone_sum.description == "256GB, Серый цвет, 200MP камера + 512GB, Gray space"
+    assert smartphone_sum.model == "S23 Ultra + 15"
+    assert smartphone_sum.color == "Серый + Gray space"
+
 
 if __name__ == "__main__":
     pytest.main()
