476090ef1ede6981db3c007f9ff9888fd60b6fb8
grayd <sharin@you-x.ru>
2024-08-07 13:01:22 +0300

    Выделил общий абстрактный класс для всех продуктов Реализовали миксин для логирования информации о создаваемом объекте

--- category.py
+++ category.py
@@ -1,31 +1,18 @@
 class Category:
-    def __init__(self, name):
+    def __init__(self, name, description, products=None):
         self.name = name
-        self._products = []
+        self.description = description
+        self._products = products if products else []
+        self._product_count = len(self._products)
 
     def add_product(self, product):
         self._products.append(product)
+        self._product_count += 1
 
     @property
     def products(self):
-        return [f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт." for product in self._products]
-
-class Product:
-    def __init__(self, name, price, quantity):
-        self.name = name
-        self.__price = price
-        self.quantity = quantity
+        return [f"{p.name}, {p.price} руб. Остаток: {p.quantity} шт." for p in self._products]
 
     @property
-    def price(self):
-        return self.__price
-
-    @price.setter
-    def price(self, new_price):
-        if new_price <= 0:
-            raise ValueError("Цена должна быть больше нуля.")
-        self.__price = new_price
-
-    @classmethod
-    def new_product(cls, product_data):
-        return cls(product_data['name'], product_data['price'], product_data['quantity'])
+    def product_count(self):
+        return self._product_count
--- product.py
+++ product.py
@@ -2,6 +2,19 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
-        #self.color = color
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            raise ValueError("Цена должна быть больше нуля.")
+        self.__price = new_price
+
+    @classmethod
+    def new_product(cls, product_data):
+        return cls(product_data['name'], product_data['description'], product_data['price'], product_data['quantity'])
