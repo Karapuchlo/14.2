e6443e3496ae087801a95277ed31b032a255de21
grayd <sharin@you-x.ru>
2024-08-15 13:11:38 +0300

    добавил обработка исключения деления на 0

--- category.py
+++ category.py
@@ -13,8 +13,11 @@ class Category:
     def middle_price(self):
         if not self.products:
             return 0
-        total_price = sum(product.price for product in self.products)
-        return total_price / len(self.products)
+        try:
+            total_price = sum(product.price for product in self.products)
+            return total_price / len(self.products)
+        except ZeroDivisionError:
+            return 0
 
     @property
     def product_count(self):
